5b9f315a7ed789dd8a896ee39021fe31d075df38
Arun-Arumugam-1 <arunaru13042008@gmail.com>
2026-04-11 15:37:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from model import predict_spam
 from link_detector import check_link
+from PLI import Image
+from image_detector import analyze_image
 
 # Page settings
 st.set_page_config(page_title="SpamShield AI", page_icon="🛡", layout="centered")
